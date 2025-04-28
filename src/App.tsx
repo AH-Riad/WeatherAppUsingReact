1ab2b3ffe3eee1182347b595dc0acffde79d43d3
Riad <riadk910@gmail.com>
2025-04-28 21:59:54 +0600

    End of Checking

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import Button from "@mui/material/Button";
+import Alert from "@mui/material/Alert";
+
 function App() {
+  let handelclick = () => {
+    console.log("Button clicked");
+  };
   return (
     <>
       <h1>Material Ui Demo</h1>
-      <Button variant="contained">CLick me!</Button>
+      <Button variant="contained" onClick={handelclick}>
+        CLick me!
+      </Button>
+      <Alert severity="error">This is an error Alert.</Alert>
     </>
   );
 }
